db3e02a54d921e27bef17c82f069aa0f42adab4f
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2017-12-03 14:22:28 +0200

    refactor Tennis

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,7 +1,7 @@
 package ohtu;
 
 public class TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -13,68 +13,73 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1")) {
             m_score1 += 1;
-        else
+        } else {
             m_score2 += 1;
+        }
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+
+        if (m_score1 == m_score2) {
+            score = equalPoints();
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            score = advantageOrWin();
+        } else {
+            score = getGameStatus();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        
+        return score;
+    }
+
+    public String equalPoints() {
+        String score = getStatus(m_score1);
+        if (score.equals("Deuce")) {
+            return score;
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        return score + "-All";
+    }
+
+    public String advantageOrWin() {
+        String score;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) {
+            score = "Advantage player1";
+        } else if (minusResult == -1) {
+            score = "Advantage player2";
+        } else if (minusResult >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
         }
         return score;
+    }
+
+    public String getStatus(int playerScore) {
+        String score;
+        switch (playerScore) {
+            case 0:
+                score = "Love";
+                break;
+            case 1:
+                score = "Fifteen";
+                break;
+            case 2:
+                score = "Thirty";
+                break;
+            case 3:
+                score = "Forty";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+        return score;
+    }
+    
+    public String getGameStatus() {
+        return getStatus(m_score1) + "-" + getStatus(m_score2);
     }
 }
